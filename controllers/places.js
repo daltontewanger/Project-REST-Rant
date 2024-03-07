57ee7e12a01e5cfd6f43f3ee08915a1ffe27285e
Dalton Tewanger <daltontewanger@yahoo.com>
2024-03-06 19:25:38 -0800

    Form data is getting through! Rejoice!

--- controllers/places.js
+++ controllers/places.js
@@ -1,26 +1,33 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/table-in-restaurant.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/coffee-cat.jpg'
-      }]
-      
-    res.render('places/index', { places })
+router.get("/new", (req, res) => {
+    res.render("places/new");
+  });
+
+router.get("/", (req, res) => {
+  let places = [
+    {
+      name: "H-Thai-ML",
+      city: "Seattle",
+      state: "WA",
+      cuisines: "Thai, Pan-Asian",
+      pic: "/images/table-in-restaurant.jpg",
+    },
+    {
+      name: "Coding Cat Cafe",
+      city: "Phoenix",
+      state: "AZ",
+      cuisines: "Coffee, Bakery",
+      pic: "/images/coffee-cat.jpg",
+    },
+  ];
+
+  res.render("places/index", { places });
+});
+
+router.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('POST /places')
 })
 
-router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
-  
-
-module.exports = router
+module.exports = router;
--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const express = require("express");
 const app = express();
 
 // Express settings
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
 
 // Controllers and Routes
 app.use('/places', require('./controllers/places'))
