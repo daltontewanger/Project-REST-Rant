71a72a61aac1602c4fa0a9c33166cd687befb95f
Dalton Tewanger <daltontewanger@yahoo.com>
2024-03-08 11:26:30 -0800

    Added Navbar with style using bootstrap

--- views/default.jsx
+++ views/default.jsx
@@ -13,11 +13,29 @@ function Def(html) {
       </head>
       <body>
         <header>
-          <h3>
-            <a href="/">HOME</a>
-          </h3>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div className="container">
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <a className="nav-link" href="/">HOME</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/places">PLACES</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/places/new">ADD PLACE</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
         </header>
         {html.children}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
       </body>
     </html>
   );
